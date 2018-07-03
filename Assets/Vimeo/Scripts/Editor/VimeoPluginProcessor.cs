ad088960d75fb748296603283333f2250d7c9c3a
Casey Pugh <me@caseypugh.com>
2018-07-03 18:05:16 -0400

    Rename Processor files

--- Assets/Vimeo/Scripts/Editor/VimeoPluginProcessor.cs
+++ Assets/Vimeo/Scripts/Editor/VimeoPluginProcessor.cs
@@ -10,7 +10,7 @@ using System.IO;
 namespace Vimeo
 {
     [InitializeOnLoad]
-    public class VimeoProcesser : AssetPostprocessor
+    public class VimeoPluginProcesser : AssetPostprocessor
     {
         static void AddPlayerDefine(string target)
         {
