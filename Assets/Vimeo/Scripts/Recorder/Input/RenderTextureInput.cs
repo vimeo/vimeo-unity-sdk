09a0a975a9dd52946bd79af7e445a9baca856ccc
Casey Pugh <me@caseypugh.com>
2018-08-29 11:15:18 -0400

    Fix spacing and combine #if

--- Assets/Vimeo/Scripts/Recorder/Input/RenderTextureInput.cs
+++ Assets/Vimeo/Scripts/Recorder/Input/RenderTextureInput.cs
@@ -1,37 +1,35 @@
-#if UNITY_2017_2_OR_NEWER
- #if UNITY_EDITOR
+#if UNITY_2017_2_OR_NEWER && UNITY_EDITOR
 
- using UnityEngine;
- using UnityEngine.Rendering;
- using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
- namespace Vimeo.Recorder
- {
-     public class RenderTextureInput : VideoInput
-     {
-         public Texture2D image;
+namespace Vimeo.Recorder
+{
+    public class RenderTextureInput : VideoInput
+    {
+        public Texture2D image;
 
-         public override Texture GetFrame()
-         {
-             image = new Texture2D(outputWidth, outputHeight, TextureFormat.RGBA32, false);
-             RenderTexture.active = recorder.renderTextureTarget;
-             image.ReadPixels(new Rect(0, 0, outputWidth, outputHeight), 0, 0);
-             image.Apply();
-             return image;
-         }
+        public override Texture GetFrame()
+        {
+            image = new Texture2D(outputWidth, outputHeight, TextureFormat.RGBA32, false);
+            RenderTexture.active = recorder.renderTextureTarget;
+            image.ReadPixels(new Rect(0, 0, outputWidth, outputHeight), 0, 0);
+            image.Apply();
+            return image;
+        }
 
-         public override void BeginRecording(){
-             outputWidth = recorder.renderTextureTarget.width;
-             outputHeight = recorder.renderTextureTarget.height;
-         }
+        public override void BeginRecording(){
+            outputWidth = recorder.renderTextureTarget.width;
+            outputHeight = recorder.renderTextureTarget.height;
+        }
 
-         public override void EndFrame()
-         {
-             Object.Destroy(image);
-             image = null;
-         }
-     }
- }
+        public override void EndFrame()
+        {
+            Object.Destroy(image);
+            image = null;
+        }
+    }
+}
 
- #endif
- #endif 
+#endif 
