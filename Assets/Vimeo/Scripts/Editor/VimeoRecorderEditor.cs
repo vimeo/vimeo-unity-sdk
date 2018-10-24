adef5fc93278a40da044a73e12e1bfa688e19156
Casey Pugh <me@caseypugh.com>
2018-10-24 15:21:11 -0400

    Fixed issue where password was not showing up next to privacy dropdown

--- Assets/Vimeo/Scripts/Editor/VimeoRecorderEditor.cs
+++ Assets/Vimeo/Scripts/Editor/VimeoRecorderEditor.cs
@@ -75,16 +75,16 @@ namespace Vimeo.Recorder
 
                     EditorGUILayout.PropertyField(so.FindProperty("videoName"));
                     EditorGUILayout.PropertyField(so.FindProperty("privacyMode"));
+                    
+                    if (VimeoApi.PrivacyModeDisplay.OnlyPeopleWithAPassword == recorder.privacyMode) {
+                        EditorGUILayout.PropertyField(so.FindProperty("videoPassword"), new GUIContent("Password"));
+                    }
 
                     GUISelectFolder();
 
                     EditorGUILayout.PropertyField(so.FindProperty("commentMode"), new GUIContent("Comments"));
                     EditorGUILayout.PropertyField(so.FindProperty("enableDownloads"));
                     EditorGUILayout.PropertyField(so.FindProperty("enableReviewPage"));
-
-                    if (VimeoApi.PrivacyModeDisplay.OnlyPeopleWithAPassword == recorder.privacyMode) {
-                        EditorGUILayout.PropertyField(so.FindProperty("videoPassword"), new GUIContent("Password"));
-                    }
 
                     EditorGUILayout.PropertyField(so.FindProperty("openInBrowser"));
 
