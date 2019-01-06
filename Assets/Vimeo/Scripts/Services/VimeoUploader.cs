4631a5f71ce5d9eac8b83c8cf9c64b6f88306c7a
Mic <boccara.michael@gmail.com>
2019-01-06 13:01:16 +0200

    Handler for upload errors

--- Assets/Vimeo/Scripts/Services/VimeoUploader.cs
+++ Assets/Vimeo/Scripts/Services/VimeoUploader.cs
@@ -16,6 +16,7 @@ namespace Vimeo
         public delegate void UploadAction(string status, float progress);
         public event UploadAction OnUploadProgress;
         public event RequestAction OnUploadComplete;
+        public event RequestAction OnUploadError;
         public event RequestAction OnUploadInit;
 
         private List<VideoChunk> m_chunks;
@@ -171,6 +172,10 @@ namespace Vimeo
             m_isUploading = false;
             if (OnChunckUploadError != null && chunk != null) {
                 OnChunckUploadError(chunk, err);
+            }
+            if (OnUploadError != null)
+            {
+                OnUploadError(err);
             }
         }
 
